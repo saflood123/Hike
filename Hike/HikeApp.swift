4004baf7b5ba13106ab9f8be8d37514e3e905c21
s flood <stuartflood@gmail.com>
2024-08-04 15:35:06 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by stuart flood on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
