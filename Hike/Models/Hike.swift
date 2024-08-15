84c41049ace4b37a57a8ec929d5ecd291ed3f554
s flood <stuartflood@gmail.com>
2024-08-15 18:45:23 +0100

    Hike changes

---
+++ Hike/Models/Hike.swift
@@ -0,0 +1,15 @@
+//
+//  Hike.swift
+//  Hike
+//
+//  Created by stuart flood on 04/08/2024.
+//
+
+import Foundation
+
+struct Identifer {
+    let id = UUID()
+    let name: String
+    let photo: String
+    let miles: Double
+}
